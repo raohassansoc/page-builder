1e4f3762118e780392a0a7b24cd80f3358d4900e
raohassansoc <rao.hassan@soc-solution.com>
2023-10-23 15:10:13 +0500

    working on run command programmatically

--- pages/_app.js
+++ pages/_app.js
@@ -1,120 +1,19 @@
 import '@/styles/globals.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Web3Storage, File } from 'web3.storage';
 import axios from 'axios';
 
 
 export default function App({ Component, pageProps }) {
-  const [uploading, setUploading] = useState(false);
-  // async function getFileList(directory) {
-  //   const fs = require('fs');
-  //   const util = require('util');
-  //   const readdir = util.promisify(fs.readdir);
-  //   const stat = util.promisify(fs.stat);
-
-  //   async function getFilesRecursively(dir) {
-  //     const files = [];
-  //     const entries = await readdir(dir);
-
-  //     for (const entry of entries) {
-  //       const entryPath = `${dir}/${entry}`;
-  //       const entryStat = await stat(entryPath);
-
-  //       if (entryStat.isDirectory()) {
-  //         const nestedFiles = await getFilesRecursively(entryPath);
-  //         files.push(...nestedFiles);
-  //       } else {
-  //         files.push(new File([fs.readFileSync(entryPath)], { path: entryPath.replace(directory, '') }));
-  //       }
-  //     }
-
-  //     return files;
-  //   }
-
-  //   return getFilesRecursively(directory);
-  // }
   const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMzNjgwREU5MDNBRUIxMjc2NEZGRDJhOWNDRUFDNTNFYjdkMzVkQkIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2OTQ1MzA5NTA3NzQsIm5hbWUiOiJidWlsZGVyIn0.4B_Mvq1WvDruzbxzU37gHS4cfKG6DVsp4dDkdo495eA'; // Replace with your API key
   const client = new Web3Storage({ token: apiKey });
 
-  const handleUploads = async () => {
-    setUploading(true);
+  const [selectedFolder, setSelectedFolder] = useState();
 
-    // Create a new Web3Storage client with your API key
-
-
-    // Replace this with the actual path to your Next.js build folder
-    const buildPath = './out'; // Adjust this as needed
-
-    try {
-      // Read the files from your build folder
-      // const files = await getFileList(buildPath);
-      console.log("Rao Hassan now")
-      const files = await axios.get("/api/get-file/route")
-
-      console.log("My object ==> ", files)
-
-
-      // Upload the files to Web3.storage
-      // const { cid } = await client.put(files);
-      // console.log(`Uploaded to IPFS with CID: ${cid}`);
-    } catch (error) {
-      console.error('Error uploading to IPFS:', error);
-    } finally {
-      setUploading(false);
-    }
-  };
-
-  async function getFilesFromDirectory(inputElement) {
-    const filesList = [];
-    const items = inputElement.files;
-
-
-    for (let i = 0; i < items.length; i++) {
-      const file = items[i];
-      filesList.push(file);
-    }
-
-
-    return filesList;
-  }
-
-
-  const uploadNextBuild = async (folder) => {
-    console.log("folder ==> ", folder)
-    // Assuming you have an input with webkitdirectory for users to select the build folder
-    const inputElement = document.getElementById('inp');
-    const allFiles = await getFilesFromDirectory(folder);
-
-
-    if (allFiles.length === 0) return;
-
-
-    try {
-      console.log('Uploading Next.js build ...');
-
-
-      // This assumes client.put() accepts an array of File objects for folders.
-      // Depending on the library/client you are using, this could vary.
-      const cid = await client.put(allFiles);
-
-
-      console.log('Next.js build uploaded');
-
-
-      const url = `https://${cid}.ipfs.w3s.link/`;
-      // Do what you need with the URL
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  // /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-  const [selectedFolder, setSelectedFolder] = useState(null);
+  useEffect(() => { }, [])
 
   const handleFolderChange = (event) => {
-    const folder = event.target.files[0];
+    const folder = event.target.files;
     setSelectedFolder(folder);
   };
 
@@ -124,35 +23,49 @@ export default function App({ Component, pageProps }) {
       return;
     }
 
-    // const folder = await axios.post("/api/upload/upload", )
-    // console.log("My Folder Response ===> ",folder )
+
     // You can handle the folder upload logic here
     // For example, you can use the selectedFolder variable to access the folder's data.
     console.log("my folder ==> ", selectedFolder)
     alert('Folder selected for upload.');
   };
 
+  async function prepareFilesFromInput() {
+    const filesArray = Array.from(selectedFolder);
+    return filesArray.map(file => {
+      const filePath = file.webkitRelativePath || file.name;
+      // console.log("file path", filePath)
+      let newpath = filePath.replace("out/", "")
+      // console.log("my new path ",newpath )
+      return new File([file], newpath, { type: file.type });
+    });
+  }
 
-  const [buildOutput, setBuildOutput] = useState('');
+  async function run() {
+    const folderForIPFS = await prepareFilesFromInput();
+    console.log(folderForIPFS)
+    // return
+    console.log("my Response ===> ", folderForIPFS)
+    console.log("Is Running")
+    const cid = await client.put(folderForIPFS);
+    console.log(`Uploaded to IPFS with CID: https://${cid}.ipfs.w3s.link`);
 
-  const runBuildCommand = async () => {
-    try {
-      const response = await fetch('/api/build/build');
-      const data = await response.json();
-      if (data.success) {
-        setBuildOutput(data.stdout);
-      } else {
-        setBuildOutput(`Error: ${data.error}`);
-      }
-    } catch (error) {
-      setBuildOutput(`Error: ${error.message}`);
-    }
-  };
+
+    const res = await client.get(cid);
+    console.log("response == > ", res)
+
+    // const filename = 'baby...dfny'; // replace this with the actual filename
+    // const content = await client.get(cid,filename);
+    // console.log("What", content);
+  }
+
+  async function createBuild() {
+    const build = await axios.get("/api/build/build")
+    console.log("my build ===> ", build)
+  }
+
 
   return <>
-    <a href="/api/download/download">Download .next folder</a>
-    {/* <button onClick={runBuildCommand}>Run npm run build</button> */}
-    <pre>{buildOutput}</pre>
     <div>
       <input
         type="file"
@@ -161,16 +74,39 @@ export default function App({ Component, pageProps }) {
         directory="true"
         onChange={handleFolderChange}
         multiple="true"
-      // style={{ display: 'none' }}
       />
-      <label htmlFor="folderInput" style={{ cursor: 'pointer' }}>
-        Select a folder
-      </label>
-      <button onClick={handleUpload}>Upload Folder</button>
+      <button onClick={run} style={{
+        display: "inline-block",
+        backgroundColor: "#0047FF",
+        color: "white",
+        padding: "5px 24px",
+        borderRadius: "6px",
+        fontSize: "16px",
+        fontWeight: 600,
+        textAlign: "center",
+        border: "none",
+        cursor: "pointer",
+        transition: "background-color 0.3s ease",
+        marginRight: "15px"
+      }}>Click</button>
+      <a href="/api/download/download">Download .next folder</a>
+      <button onClick={createBuild} style={{
+        display: "inline-block",
+        backgroundColor: "#0047FF",
+        color: "white",
+        padding: "5px 24px",
+        borderRadius: "6px",
+        fontSize: "16px",
+        fontWeight: 600,
+        textAlign: "center",
+        border: "none",
+        cursor: "pointer",
+        transition: "background-color 0.3s ease",
+        marginLeft: "5px",
+        marginRight: "15px"
+      }}>Create Build</button>
+      {/* <button onClick={handleUpload}>Upload Folder</button> */}
     </div>
 
-    {/* <input type='file' webkitdirectory id='inp' onChange={(e) => uploadNextBuild(e.target.value)} /> */}
-    {/* <button onClick={uploadNextBuild}>check </button> */}
-    <button onClick={handleUploads}>Button</button>
     <Component {...pageProps} /></>
 }
--- pages/api/build/build.js
+++ pages/api/build/build.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
 import { exec } from 'child_process';
-import { promisify } from 'util';
-
-const execAsync = promisify(exec);
+// import util from 'util';
+// const execAsync = util.promisify(exec);
 
 export default async function handler(req, res) {
+  console.log("Starting build process....");
+
   try {
-    const { stdout, stderr } = await execAsync('npm run build');
-    res.status(200).json({ success: true, stdout, stderr });
+
+    exec('npm run build', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      }
+    });
+
+    res.status(200).json({ success: true });
   } catch (error) {
+    console.error("Error during build:", error.message);
     res.status(500).json({ success: false, error: error.message });
   }
 }
